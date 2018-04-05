2a3a9056b66aeccc9001ea21527781628ca7a2a3
Phong Ho <ho.phongt@gmail.com>
2018-04-05 02:15:20 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,3 +2,19 @@
 function paragraphSelector(){
   return $('p');
 }
+
+function lastImageSelector(){
+  
+}
+
+function ninjaBabySelector(){
+  
+}
+
+function divSelector(){
+  
+}
+
+function firstListItem(){
+  
+}
